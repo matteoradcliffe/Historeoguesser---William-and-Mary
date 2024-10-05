c6badf525125a77ea19bfe0db5a55020b2f340df
MmmmmSpicy <ethanspickard@gmail.com>
2024-10-05 13:19:46 -0400

    Added distance calculation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+let guess_location
+let random_location
 
 
 async function initialize() {
-     // Request needed libraries.
+    
     const pois = [
          {lat: 37.2702364, lng: -76.7144971}, //ISC
           {lat: 37.2712101, lng: -76.7195004}, //Pleasants
@@ -10,10 +12,11 @@ async function initialize() {
           {lat: 37.2708312, lng: -76.7073767} //DoG
     ]
 
-    const guess_location = pois[Math.floor((Math.random() * (pois.length)))]
+    const random_location = pois[Math.floor((Math.random() * (pois.length)))]
 
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    
     const myLatLng = { lat: 37.2706829, lng: -76.7159741 };
     const map = new Map(document.getElementById("minimap"), {
         center: myLatLng,
@@ -23,7 +26,8 @@ async function initialize() {
         fullscreenControl: false,
         mapId: "4504f8b37365c3d0",
     });
-    const marker = new AdvancedMarkerElement({
+
+    let marker = new AdvancedMarkerElement({
         map,
         position: { lat: 37.2706829, lng: -76.7159741 },
     });
@@ -32,7 +36,7 @@ async function initialize() {
         document.getElementById("map"),
           {
             
-            position: guess_location,
+            position: random_location,
         
             zoomControlOptions: {
                 position: google.maps.ControlPosition.TOP_LEFT,
@@ -46,17 +50,35 @@ async function initialize() {
             clickToGo: false
             
           },
-        );
+    );
 
     map.addListener("click", (mapsMouseEvent) => {
         marker.setMap(null)
+        guess_location = mapsMouseEvent.latLng
         marker = new AdvancedMarkerElement({
             map,
             position: mapsMouseEvent.latLng
+            
         });
-    
+        
     });
-    console.log(guess_location)
-  }
-  
+    
+
+}
+
+function calculate_distance() {
+    var randomLat = random_location.lat
+    var randomLng = random_location.lng
+    var guessLat = guess_location.lat()
+    var guessLng = guess_location.lng()
+
+    var metersLat = Math.abs(randomLat - guessLat) * 111.111
+    var metersLng = Math.abs(randomLng - guessLng) * 111.111
+
+    return Math.sqrt(Math.pow(metersLat, 2) + Math.pow(metersLng, 2))
+    
+}
+
 initialize();
+
+
